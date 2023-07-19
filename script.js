9911e10d9b56ec72267d2cc6162fe55d91be608e
cadeci <ciaracade03@gmail.com>
2023-07-19 04:33:31 -0400

    Working RPS Game

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const options = ["Rock", "Paper", "Scissors"];
+const options = ["Sword", "Shield", "Bow"];
 const optionsBeat = new Map([
-    ["rock", "paper"],
-    ["paper", "scissors"],
-    ["scissors", "rock"]
+    ["sword", "bow"],
+    ["bow", "shield"],
+    ["shield", "sword"]
 ]);
 
+const buttons = document.querySelectorAll('.Btn');
+let results = document.getElementById("resultsSpan");
+let rounds = document.getElementById("round");
+let round = 0;
+
+let playerScore = document.getElementById("playerScore");
+let computerScore = document.getElementById("computerScore");
+let playerScoreNum = 0;
+let computerScoreNum = 0;
+
+
+
 function getComputerChoice() {
-    return options[Math.floor(Math.random() * options.length)]
+    return options[Math.floor(Math.random() * options.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -14,29 +26,47 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerSelection.toLowerCase();
     let messageString = "";
 
+    if(round < 5){
     if (playerSelection === optionsBeat.get(computerSelection)) {
-        messageString = "You win! " + playerSelection + " beats " + computerSelection + "."
+        messageString = "You win! " + playerSelection + " beats " + computerSelection + ".";
+        playerScoreNum += 1;
 
     } else if (computerSelection === optionsBeat.get(playerSelection)) {
-        messageString = "You lose! " + computerSelection + " beats " + playerSelection + "."
+        messageString = "You lose! " + computerSelection + " beats " + playerSelection + ".";
+        computerScoreNum += 1;
     } else if (computerSelection === playerSelection) {
         messageString = "Tie! No winners."
     }
+    round += 1;
 
-    return messageString;
+
+    playerScore.textContent = playerScoreNum;
+    computerScore.textContent = computerScoreNum;
+    rounds.textContent = "Round " + round;
+   results.textContent = messageString;
+} else {
+    if (computerScoreNum>playerScoreNum){
+        messageString = "Computer wins! ";
+     } else if (playerScoreNum>computerScoreNum){
+        messageString = "Player wins! ";
+     } else {
+        messageString = "Tie! ";
+     }
+    results.textContent = messageString + "Reload page to play again.";
+
 }
 
 
-function game() {
-    var round = 0
-
-    while (round < 5) {
-        const playerSelection = prompt();
-        const computerSelection = getComputerChoice();
-        round += 1;
-        console.log(playRound(playerSelection, computerSelection));
-    }
 }
 
-console.log(game());
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const img = button.querySelector("img");
+        playerSelection = img.alt.toLowerCase();
+        playRound(playerSelection, getComputerChoice());
+    });
+});
+
 
